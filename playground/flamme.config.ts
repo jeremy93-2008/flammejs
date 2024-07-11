9e17f0c991b49fa0f8e0e87606d852c730e0d003
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2024-07-11 19:04:16 +0200

    Initial commit

---
+++ playground/flamme.config.ts
@@ -0,0 +1,11 @@
+export default {
+    clientDir: 'src/client',
+    serverDir: 'src/server',
+    serverBasePath: '/api',
+
+    buildDir: 'dist',
+    publicDir: 'public',
+    publicPath: '/',
+
+    devServerPort: 3000,
+}
---
+++ playground/src/client/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Index() {
+    return (
+        <div>
+            <h1>Hello, Flamme!</h1>
+            <p>This is a React component in <code>src/client/index.tsx</code>.</p>
+            <p>It is rendered SSR by the server and hydrated by the client.</p>
+        </div>
+    )
+}
