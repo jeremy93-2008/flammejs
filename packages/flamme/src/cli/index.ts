134c6248f1873d18fd47ba95c1e341b4b0f59c20
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2024-07-30 23:33:09 +0200

    bump: new alpha version for every packages

--- packages/flamme/src/cli/index.ts
+++ packages/flamme/src/cli/index.ts
@@ -11,7 +11,7 @@ loadEnv().then()
 const main = defineCommand({
     meta: {
         name: 'flamme',
-        version: '1.0.0',
+        version: '0.0.1',
         description: 'Flamme CLI',
     },
     subCommands: {
