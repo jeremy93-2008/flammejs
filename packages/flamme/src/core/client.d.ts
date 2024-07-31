f60a9db80ab78c64e1574313dfe0e6a88a14b909
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2024-07-31 09:55:03 +0200

    feat: add types for modules in templates

---
+++ packages/flamme/src/core/client.d.ts
@@ -0,0 +1,116 @@
+// CSS modules
+type CSSModuleClasses = { readonly [key: string]: string }
+
+declare module '*.module.css' {
+    const classes: CSSModuleClasses
+    export default classes
+}
+declare module '*.module.scss' {
+    const classes: CSSModuleClasses
+    export default classes
+}
+declare module '*.module.sass' {
+    const classes: CSSModuleClasses
+    export default classes
+}
+declare module '*.module.less' {
+    const classes: CSSModuleClasses
+    export default classes
+}
+declare module '*.module.styl' {
+    const classes: CSSModuleClasses
+    export default classes
+}
+declare module '*.module.stylus' {
+    const classes: CSSModuleClasses
+    export default classes
+}
+
+// CSS
+declare module '*.css' {}
+declare module '*.scss' {}
+declare module '*.sass' {}
+declare module '*.less' {}
+declare module '*.styl' {}
+declare module '*.stylus' {}
+
+// Assets
+declare module '*.svg' {
+    const src: string
+    export default src
+}
+
+declare module '*.jpg' {
+    const src: string
+    export default src
+}
+
+declare module '*.gif' {
+    const src: string
+    export default src
+}
+
+declare module '*.png' {
+    const src: string
+    export default src
+}
+
+declare module '*.woff' {
+    const src: string
+    export default src
+}
+
+declare module '*.woff2' {
+    const src: string
+    export default src
+}
+
+declare module '*.eot' {
+    const src: string
+    export default src
+}
+
+declare module '*.ttf' {
+    const src: string
+    export default src
+}
+
+declare module '*.otf' {
+    const src: string
+    export default src
+}
+
+declare module '*.mp4' {
+    const src: string
+    export default src
+}
+
+declare module '*.webm' {
+    const src: string
+    export default src
+}
+
+declare module '*.wav' {
+    const src: string
+    export default src
+}
+
+declare module '*.mp3' {
+    const src: string
+    export default src
+}
+
+declare module '*.m4a' {
+    const src: string
+    export default src
+}
+
+declare module '*.aac' {
+    const src: string
+    export default src
+}
+
+declare module '*.oga' {
+    const src: string
+    export default src
+}
