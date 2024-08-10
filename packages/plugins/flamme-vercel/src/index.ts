2e13b9dcfc517e3f55574164ae161fbfe2bfa6ba
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2024-08-10 20:39:34 +0200

    feat: create preparing deployment script for vercel for flamme

--- packages/plugins/flamme-vercel/src/index.ts
+++ packages/plugins/flamme-vercel/src/index.ts
@@ -4,6 +4,11 @@ import { formatShortDate } from './utils/formatShortDate'
 import { spawnSync } from 'child_process'
 import { mkdirSync } from 'fs'
 import { useFlammeConfig } from './hooks/useFlammeConfig'
+import * as path from 'node:path'
+import * as fsExtra from 'fs-extra'
+import { existsSync, writeJSONSync } from 'fs-extra'
+
+const defaultBuildFolderName = '.flamme'
 
 const main = defineCommand({
     meta: {
@@ -36,11 +41,11 @@ const main = defineCommand({
                 colors.white('[flamme-vercel]'),
                 'Building your Flamme app...'
             )
-            spawnSync('flamme build --buildDir=.build', {
+            buildDir = defaultBuildFolderName
+            spawnSync(`flamme build --buildDir=${buildDir}`, {
                 shell: true,
                 stdio: 'inherit',
             })
-            buildDir = '.build'
         }
 
         console.log(
@@ -49,7 +54,73 @@ const main = defineCommand({
             'Preparing your Flamme app to Vercel...'
         )
 
+        if (existsSync('.vercel')) {
+            fsExtra.removeSync('.vercel')
+        }
+
+        console.log(
+            formatShortDate(new Date()),
+            colors.white('[flamme-vercel]'),
+            'Creating the Vercel structure...'
+        )
+
+        // We create the base structure for Vercel
         mkdirSync('.vercel/output', { recursive: true })
+        // We create the static directory
+        mkdirSync('.vercel/output/static', { recursive: true })
+        // We create the functions directory
+        mkdirSync('.vercel/output/functions', { recursive: true })
+
+        console.log(
+            formatShortDate(new Date()),
+            colors.white('[flamme-vercel]'),
+            'Copying files...'
+        )
+
+        // We create the vercel config file
+        writeJSONSync('.vercel/output/config.json', {
+            version: 3,
+        })
+
+        //We copy the build directory to the static directory, but no server files
+        fsExtra.copySync(
+            path.resolve(currentDirectory, buildDir),
+            '.vercel/output/static',
+            {
+                filter: (src, dest) => {
+                    return !src.includes('server')
+                },
+            }
+        )
+
+        // We create the single function directory for Vercel compatibility
+        mkdirSync('.vercel/output/functions/index.func', { recursive: true })
+
+        // Read the manifest file to get the hash
+        const buildManifest = await import(
+            path.resolve(currentDirectory, buildDir, '_buildManifest.js')
+        )
+        const hashKey = buildManifest.default.hashKey
+
+        // We create the vc config file for Vercel
+        writeJSONSync('.vercel/output/functions/index.func/.vc-config.json', {
+            handler: `server.${hashKey}.js`,
+            runtime: 'nodejs',
+            launcherType: 'Nodejs',
+            supportsResponseStreaming: true,
+        })
+
+        // We copy the server files to the functions directory
+        fsExtra.copySync(
+            path.resolve(currentDirectory, buildDir),
+            '.vercel/output/functions/index.func'
+        )
+
+        console.log(
+            formatShortDate(new Date()),
+            colors.white('[flamme-vercel]'),
+            'Vercel structure created!'
+        )
     },
 })
 
